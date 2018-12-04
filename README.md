c348603c36d77fd61c11ef4d45db4da5285e8b61
kq <kq@qq.com>
2018-12-05 07:33:08 +0800

    编写markdown说明

--- README.md
+++ README.md
@@ -1,2 +1,161 @@
 # markdown
-自己动手做个markdown,web编辑器；Do it yourself, markdown, web editor；
+自己动手做个markdown,web编辑器；Do it yourself, markdown, web editor,瞎打的英文。
+### 如何使用
+> 文件说明
+```
+drwxr-xr-x 1 kongqi 197121    0 12月  5 06:33 css/	//样式
+-rw-r--r-- 1 kongqi 197121  944 12月  5 06:55 iframe.html //iframe例子
+-rw-r--r-- 1 kongqi 197121 4460 12月  5 06:55 iframe_editor.html iframe使用编辑器文件
+drwxr-xr-x 1 kongqi 197121    0 12月  5 06:37 images/ 图片图标
+-rw-r--r-- 1 kongqi 197121 4585 12月  5 06:55 index.html
+drwxr-xr-x 1 kongqi 197121    0 12月  5 06:32 js/	
+-rw-r--r-- 1 kongqi 197121 1084 12月  5 07:05 LICENSE
+-rw-r--r-- 1 kongqi 197121   95 12月  5 07:05 README.md
+
+```
+1. js/KqEditor.js，主要的编辑器文件  
+2. 依赖jquery,marked,ace  
+
+> 初始化
+```
+<script type="text/javascript">
+			var editor = KqEditor({
+				obj: 'editor',//编辑器实力的对象，这里不需要加#
+				cacheCover: 1,
+				viewObj: "#preview",//预览对象的HTML id
+				scrollView: "#preview-warp",//预览对象滚动条的HTML id
+				localInput: "#edinput", //本地设置把编辑器内容同步到input里面
+				//theme:"ace/theme/tomorrow",
+				//上传图片自己写,ed返回的时编辑器的实例,最后用ed.insert('内容');
+				// 				uploadImage:function(ed){
+				// 					
+				// 				}
+			});
+			//editor.setContentInput();
+		</script>
+```
+> 引入文件
+1.style 
+```
+	<link rel="stylesheet" type="text/css" href="http://www.heibaiketang.com/style/bootstrap/css/bootstrap.css"><!--bootstrap样式，我这里用了我自己编译过的，不使用也可以-->
+	<link href="http://cdn.bootcss.com/highlight.js/8.0/styles/monokai_sublime.min.css" rel="stylesheet"><!--ACE编辑器样式-->
+	<link rel="stylesheet" type="text/css" href="css/editor.css" /><!--编辑器必选样式-->
+```
+2.js
+```
+<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+<script src="http://cdn.bootcss.com/highlight.js/8.0/highlight.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.1/ace.js"></script>
+<script src="https://cdn.bootcss.com/marked/0.5.1/marked.min.js"></script>
+```
+3.html 编辑器
+```
+<body class="kq-editor">
+		<div class="ui-layout-north">
+			<span id="toolbar" class="noselect" data-open-title="Hide Toolbar" data-closed-title="Show Toolbar">
+				<i title="Bold" class="svg-icon svg-bold styling-icon" data-type="setStyle" data-modifier="****">
+				</i>
+				<i title="Italic" class="svg-icon svg-italic styling-icon" data-type="setStyle" data-modifier="**">
+				</i>
+				<i title="Strikethrough" class="svg-icon svg-strikethrough styling-icon" data-type="setStyle" data-modifier="~~">
+				</i>
+				<i title="Insert" class="svg-icon svg-underline styling-icon" data-type="setStyleU" data-modifier="u">
+				</i>
+
+				<i class="dividor"></i>
+				<i title="Heading 1" class="svg-icon svg-heading1 heading-icon" data-type="title" data-modifier="1">
+				</i>
+				<i title="Heading 2" class="svg-icon svg-heading2 heading-icon" data-type="title" data-modifier="2">
+				</i>
+				<i title="Heading 3" class="svg-icon svg-heading3 heading-icon" data-type="title" data-modifier="3">
+				</i>
+				<i title="Heading 4" class="svg-icon svg-heading4 heading-icon" data-type="title" data-modifier="4">
+				</i>
+				<i title="Heading 5" class="svg-icon svg-heading5 heading-icon" data-type="title" data-modifier="5">
+				</i>
+				<i title="Heading 6" class="svg-icon svg-heading6 heading-icon" data-type="title" data-modifier="6">
+				</i>
+				<i class="dividor"></i>
+				<i title="Horizontal rule" class="svg-icon svg-hr" data-type="br" data-modifier="***">
+				</i>
+				<i title="Quote" class="svg-icon svg-quote list-icon" data-type="setStyleNoEnd" data-modifier="> ">
+				</i>
+				<i title="Unordered list" class="svg-icon svg-ul list-icon" data-type="setStyleNoEnd" data-modifier="-">
+				</i>
+				<i title="Ordered list" class="svg-icon svg-ol list-icon" data-type="setStyleNoEnd" data-modifier="1. ">
+				</i>
+
+				<i class="dividor"></i>
+				<i title="Link" class="svg-icon svg-link" id="link-icon" data-type="link" data-modifier="[提示文本](链接地址)">
+				</i>
+				<i title="Image" class="svg-icon svg-image" id="image-icon" data-type="image">
+				</i>
+				<i title="Code" class="svg-icon svg-code" data-type="code" data-modifier="">
+				</i>
+				<i title="Table" class="svg-icon svg-table" id="table-icon" data-type="table" data-modifier="">
+				</i>
+				<i class="dividor"></i>
+
+				<!-- <i title="theme" class="svg-icon svg-theme theme-icon">
+				</i> -->
+				
+			</span>
+		</div>
+
+		<div class="ui-layout-toggler" id="north-toggler"></div>
+		<div class="ui-layout-south">
+			<div class="ui-layout-center">
+				<div id="editor"></div> <!-- 编辑器 -->
+			</div>
+			<div class="ui-layout-toggler" id="east-toggler"></div>
+			<div class="ui-layout-east" id="preview-warp">
+				<article class="markdown-body" id="preview" data-open-title="Hide Preview" data-closed-title="Show Preview"></article>
+				<!-- 实时预览 -->
+			</div>
+		</div>
+
+		<h4>编辑器内容</h4>
+		<textarea rows="6" cols="" class="form-control" id="edinput"></textarea>
+	</body>
+```
+前面ui-layout-north这个时编辑器的工具html,如果不想要，直接删除。绑定工具哪里去掉或者是忽略
+
+### 获得编辑器内容
+```
+content=editor.getContent();//函数获取
+console.log(content);
+console.log(editor.markContent);//属性获取
+```
+### iframe里面如何使用呢
+```
+<div class="iframe">
+
+			<iframe id="fm" name="myFrame" src="iframe_editor.html" frameborder="0" height="500" width="100%"></iframe>
+			<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+
+		</div>
+		<div class="container-fluid">
+			<h4>编辑器内容</h4>
+			<textarea rows="6" cols="" class="form-control" id="markdownValue"></textarea>
+		</div>
+```
+直接应用即可，在iframe_editor.html配置下参数即可试下。
+```
+<script type="text/javascript">
+			var editor = KqEditor({
+				obj: 'editor',
+				cacheCover: 1,
+				viewObj: "#preview",
+				scrollView: "#preview-warp",
+				parentInput: '#markdownValue', //就是这里了。默认我写了这个。
+				//theme:"ace/theme/tomorrow",
+				//上传图片自己写,ed返回的时编辑器的实例,最后用ed.insert('内容');
+				// 				uploadImage:function(ed){
+				// 					
+				// 				}
+			});
+			//editor.setContentInput();
+		</script>
+```
+本文为原创，作者主页：[黑白课堂](http://www.heibaiketang.com)www.heibaiketang.com。
+演示地址:[演示进入](http://www.heibaiketang.com/tools/markdown)www.heibaiketang.com/tools/markdown
--- js/KqEditor.js
+++ js/KqEditor.js
@@ -11,15 +11,15 @@
 			viewObj: "#md-viewer",
 			cacheName: 'content',
 			highCodeObj: 'code',
-			cacheCover: 1,//缓存覆盖内容，0表示不启动缓存
+			cacheCover: 1, //缓存覆盖内容，0表示不启动缓存
 			markjs: 'marker',
 			scrollView: "",
-			tools:"#toolbar",
-			
-			uploadImage:null,//自定义上传图片
-			parentInput:'#markdownValue',//这里配置iframe时的input内容赋值，自动赋值。，
-			localInput:""//本地内容赋值,如果时本地，则iframe无效
-			
+			tools: "#toolbar",
+
+			uploadImage: null, //自定义上传图片
+			parentInput: '', //这里配置iframe时的input内容赋值，自动赋值。，
+			localInput: "" //本地内容赋值,如果时本地，则iframe无效
+
 		};
 		//判断是否使用函数形式调用，不使用new的时候 
 		if (!(this instanceof KqEditor)) {
@@ -31,7 +31,7 @@
 				this.option[k] = options[k];
 			}
 		}
-		
+
 		this.init();
 		return this;
 	}
@@ -39,9 +39,9 @@
 
 	_.init = function() {
 		var md_editor = ace.edit(this.option.obj);
-		this.editor=md_editor;
+		this.editor = md_editor;
 		md_editor.setOptions({
-			highlightActiveLine: true,//高亮
+			highlightActiveLine: true, //高亮
 			highlightSelectedWord: true,
 			enableBasicAutocompletion: true,
 			enableSnippets: true,
@@ -54,51 +54,51 @@
 		md_editor.getSession().setTabSize(this.option.tabNumber); // 设置 Tab 为４个空格
 		md_editor.getSession().setUseWrapMode(this.option.autoBr); // 自动换行
 		//如果编程语言是html，就开启emment
-		if(this.option.mode=='ace/mode/html')
-		{
+		if (this.option.mode == 'ace/mode/html') {
 			md_editor.setOption("enableEmmet", true);
 		}
-		
+
 		var that = this;
 		//文档更改时触发。
 		md_editor.getSession().on('change', function(e) {
 			//console.log(e)
 			that.parseMark(md_editor);
 			that.highCode();
-			
-			
-			
+
+
+
 		});
 		//判断缓存是否存在，如果存在调用缓存
 		this.loadingData(md_editor);
 		this.scrollBar(md_editor);
 		this.tools(md_editor);
-		
+
 		//return this;
 
 	}
 	//取得内容，可以直接调用这个函数获取。
-	_.getContent=function(){
+	_.getContent = function() {
 		return this.markContent;
-		
+
 	}
-	_.setContentInput=function(a){
+	_.setContentInput = function(a) {
 		//window.parent.body.find(a).val(this.markContent);
 		parent.$(a).val(this.getContent());
 	}
-	_.localInput=function(){
-		$(this.option.localInput).val(this.getContent());
+	_.localInput = function() {
+		if (this.option.localInput) {
+			$(this.option.localInput).val(this.getContent());
+		}
+
 	}
-	_.autoParentInput=function(){
-		if(!this.option.localInput)
-		{
-			if(this.option.parentInput)
-			{
+	_.autoParentInput = function() {
+		if (!this.option.localInput) {
+			if (this.option.parentInput) {
 				parent.$(this.option.parentInput).val(this.editor.getValue());
 			}
 		}
-		
-		
+
+
 	}
 	_.loadingData = function(md_editor) {
 		if (this.option.cacheCover) {
@@ -112,7 +112,7 @@
 	}
 	_.parseMark = function(md_editor) {
 		var content = md_editor.getValue();
-		this.markContent=content;
+		this.markContent = content;
 		this.setContentInput();
 		this.localInput();
 		this.autoParentInput();
@@ -120,9 +120,9 @@
 		this.view(content);
 	}
 	_.markData = function(data) {
-// 		if (this.option.markjs == 'markdown') {
-// 			return markdown.toHTML(data);
-// 		}
+		// 		if (this.option.markjs == 'markdown') {
+		// 			return markdown.toHTML(data);
+		// 		}
 		return marked(data);
 	}
 	_.view = function(content) {
@@ -149,18 +149,18 @@
 		var session = md_editor.getSession();
 		var that = this;
 		var scrollView = $(that.option.scrollView); // 文档预览框
-		var scrollHeight=scrollView.prop('scrollHeight');//获得预览的滚动条长度
+		var scrollHeight = scrollView.prop('scrollHeight'); //获得预览的滚动条长度
 		session.on('changeScrollTop', function() {
 			var editorTop = session.getScrollTop();
 			// 设置预览框的滚动条
-			var scrollViewTop=scrollView.scrollTop();
+			var scrollViewTop = scrollView.scrollTop();
 			//console.log(editorTop);
 			//判断是否负数
 			//var nowScroll=scrollHeight-scrollViewTop;
 			scrollView.scrollTop(editorTop);
-			
+
 		});
-		
+
 
 		// 预览框定滚动事件
 		scrollView.on('scroll', function() {
@@ -170,165 +170,152 @@
 			//var t=md_editor.getScrollBar.setInnerHeight;
 			//console.log(scrollHeight);
 			//session.setScrollTop(sTop);
-			
+
 		});
 	}
-	_.tools=function(md_editor){
-		var tools_item=$(this.option.tools+' i');
-		var that=this;
-		tools_item.on('click',function(){
+	_.tools = function(md_editor) {
+		var tools_item = $(this.option.tools + ' i');
+		var that = this;
+		tools_item.on('click', function() {
 			//获得当前的选择
-			var st=md_editor.getCursorPosition();
-			var sr=md_editor.getSelectionRange();
+			var st = md_editor.getCursorPosition();
+			var sr = md_editor.getSelectionRange();
 			//console.log('getCursorPosition',st,'getSelectionRange',sr);
-			var data=$(this).data('modifier');
-			var type=$(this).data('type');
-			type=type || '';
+			var data = $(this).data('modifier');
+			var type = $(this).data('type');
+			type = type || '';
 			//标题设置
-			if(type=='title')
-			{
-						/*
-						getCursorPosition()//获取游标的当前位置。{row: 3, column: 3}column: 3row: 3__proto__: Object }
-						getSelectionRange()返回所选文本的内容的返回Range
-						{end: {row: 3, column: 1},start: {row: 3, column: 0}}
-						navigateTo(Number row, Number column)//将光标移动到指定的行和列。请注意，这会取消选择当前选择
-						moveCursorToPosition(ops)将光标移动到所指示的位置pos.row和pos.column。
-						replace(String replacement, Object options)
-						getTextRange(Range range)给定文档中的范围，此函数将该范围内的所有文本作为单个字符串返回
-						clearSelection（）清空选择（通过取消选择）。此功能也会发出'changeSelection'事件。
-						*/
-				
-			
-                        n = md_editor.getCursorPosition();//获取游标的当前位置。
-                    	n.column += data + 1;//第几列
-						//设置位置
-                    	md_editor.navigateTo(md_editor.getSelectionRange().start.row, 0);//将光标移动到指定的行和列。请注意，这会取消选择当前选择。
-                    	data="#".repeat(data) + " ";
-						md_editor.insert(data);//光标位置插入
-                    	md_editor.moveCursorToPosition(n);
-                    	md_editor.focus(); 
-						data='';
-						
-                    	//b.event("MdHeader")
-				
-			}
-			if(type=='setStyle')
-			{
-				//取得所选位置
-				n = md_editor.getSelectionRange();
-				//获取游标的当前位置
-				r = md_editor.getCursorPosition(); 
-				r.column += data.length;//当前的游标位置+字符的长度，等待新的游标位置
-				var new_str=data+md_editor.session.getTextRange(n)+data;
-				md_editor.session.replace(n,new_str);
-				md_editor.moveCursorToPosition(r); 
-				md_editor.selection.clearSelection(); 
+			if (type == 'title') {
+				/*
+				getCursorPosition()//获取游标的当前位置。{row: 3, column: 3}column: 3row: 3__proto__: Object }
+				getSelectionRange()返回所选文本的内容的返回Range
+				{end: {row: 3, column: 1},start: {row: 3, column: 0}}
+				navigateTo(Number row, Number column)//将光标移动到指定的行和列。请注意，这会取消选择当前选择
+				moveCursorToPosition(ops)将光标移动到所指示的位置pos.row和pos.column。
+				replace(String replacement, Object options)
+				getTextRange(Range range)给定文档中的范围，此函数将该范围内的所有文本作为单个字符串返回
+				clearSelection（）清空选择（通过取消选择）。此功能也会发出'changeSelection'事件。
+				*/
+
+
+				n = md_editor.getCursorPosition(); //获取游标的当前位置。
+				n.column += data + 1; //第几列
+				//设置位置
+				md_editor.navigateTo(md_editor.getSelectionRange().start.row, 0); //将光标移动到指定的行和列。请注意，这会取消选择当前选择。
+				data = "#".repeat(data) + " ";
+				md_editor.insert(data); //光标位置插入
+				md_editor.moveCursorToPosition(n);
 				md_editor.focus();
-				data='';
-				
+				data = '';
+
+				//b.event("MdHeader")
+
 			}
-			if(type=='setStyleNoEnd')
-			{
+			if (type == 'setStyle') {
 				//取得所选位置
 				n = md_editor.getSelectionRange();
 				//获取游标的当前位置
-				r = md_editor.getCursorPosition(); 
-				r.column += data.length;//当前的游标位置+字符的长度，等待新的游标位置
-				var new_str=data+md_editor.session.getTextRange(n)
-				md_editor.session.replace(n,new_str);
-				md_editor.moveCursorToPosition(r); 
-				md_editor.selection.clearSelection(); 
+				r = md_editor.getCursorPosition();
+				r.column += data.length; //当前的游标位置+字符的长度，等待新的游标位置
+				var new_str = data + md_editor.session.getTextRange(n) + data;
+				md_editor.session.replace(n, new_str);
+				md_editor.moveCursorToPosition(r);
+				md_editor.selection.clearSelection();
 				md_editor.focus();
-				data='';
-				
+				data = '';
+
 			}
-			if(type=='setStyleU')
-			{
-				
+			if (type == 'setStyleNoEnd') {
 				//取得所选位置
 				n = md_editor.getSelectionRange();
 				//获取游标的当前位置
-				r = md_editor.getCursorPosition(); 
-				r.column += data.length+2;//当前的游标位置+字符的长度，等待新的游标位置
-				var new_str='<'+data+'>'+md_editor.session.getTextRange(n)+'</'+data+'>';
-				md_editor.session.replace(n,new_str);
-				md_editor.moveCursorToPosition(r); 
-				md_editor.selection.clearSelection(); 
+				r = md_editor.getCursorPosition();
+				r.column += data.length; //当前的游标位置+字符的长度，等待新的游标位置
+				var new_str = data + md_editor.session.getTextRange(n)
+				md_editor.session.replace(n, new_str);
+				md_editor.moveCursorToPosition(r);
+				md_editor.selection.clearSelection();
 				md_editor.focus();
-				data='';
-				
+				data = '';
+
 			}
-			if(type=='link')
-			{
+			if (type == 'setStyleU') {
+
 				//取得所选位置
 				n = md_editor.getSelectionRange();
 				//获取游标的当前位置
-				r = md_editor.getCursorPosition(); 
-				r.column += data.length;//当前的游标位置+字符的长度，等待新的游标位置
-				var new_str='['+md_editor.session.getTextRange(n)+'](链接地址)'
-				md_editor.session.replace(n,new_str);
-				md_editor.moveCursorToPosition(r); 
-				md_editor.selection.clearSelection(); 
+				r = md_editor.getCursorPosition();
+				r.column += data.length + 2; //当前的游标位置+字符的长度，等待新的游标位置
+				var new_str = '<' + data + '>' + md_editor.session.getTextRange(n) + '</' + data + '>';
+				md_editor.session.replace(n, new_str);
+				md_editor.moveCursorToPosition(r);
+				md_editor.selection.clearSelection();
 				md_editor.focus();
-				data='';
-				
+				data = '';
+
 			}
-			if(type=='code')
-			{
+			if (type == 'link') {
 				//取得所选位置
 				n = md_editor.getSelectionRange();
 				//获取游标的当前位置
-				r = md_editor.getCursorPosition(); 
-				if(r.column==0)
-				{
-					
-					var new_str='```\n'+md_editor.session.getTextRange(n)+'\n```'
-					r.row+=1;
-				}else
-				{
-					var new_str='\n```\n'+md_editor.session.getTextRange(n)+'\n```'
-					r.row+=2;
+				r = md_editor.getCursorPosition();
+				r.column += data.length; //当前的游标位置+字符的长度，等待新的游标位置
+				var new_str = '[' + md_editor.session.getTextRange(n) + '](链接地址)'
+				md_editor.session.replace(n, new_str);
+				md_editor.moveCursorToPosition(r);
+				md_editor.selection.clearSelection();
+				md_editor.focus();
+				data = '';
+
+			}
+			if (type == 'code') {
+				//取得所选位置
+				n = md_editor.getSelectionRange();
+				//获取游标的当前位置
+				r = md_editor.getCursorPosition();
+				if (r.column == 0) {
+
+					var new_str = '```\n' + md_editor.session.getTextRange(n) + '\n```'
+					r.row += 1;
+				} else {
+					var new_str = '\n```\n' + md_editor.session.getTextRange(n) + '\n```'
+					r.row += 2;
 				}
-				r.column +=0;//当前的游标位置+字符的长度，等待新的游标位置
-				md_editor.session.replace(n,new_str);
-				md_editor.moveCursorToPosition(r); 
-				md_editor.selection.clearSelection(); 
+				r.column += 0; //当前的游标位置+字符的长度，等待新的游标位置
+				md_editor.session.replace(n, new_str);
+				md_editor.moveCursorToPosition(r);
+				md_editor.selection.clearSelection();
 				md_editor.focus();
-				data='';
-				
+				data = '';
+
 			}
-			
-			
-			if(type.indexOf(['table'])!=-1)
-			{
-				data='\nheader 1 | header 2\n ---|---\n row 1 col 1 | row 1 col 2\n row 2 col 1 | row 2 col 2\n\n';
+
+
+			if (type.indexOf(['table']) != -1) {
+				data = '\nheader 1 | header 2\n ---|---\n row 1 col 1 | row 1 col 2\n row 2 col 1 | row 2 col 2\n\n';
 			}
-			
-			if(type.indexOf(['image'])!=-1)
-			{
-				if(!that.option.uploadImage)
-				{
-					data='![显示文本](图片链接地址)';
-				}else
-				{
+
+			if (type.indexOf(['image']) != -1) {
+				if (!that.option.uploadImage) {
+					data = '![显示文本](图片链接地址)';
+				} else {
 					that.uploadImg();
 				}
 			}
-			if(data)
-			{
-				md_editor.insert(data);//光标位置插入
+			if (data) {
+				md_editor.insert(data); //光标位置插入
 			}
-			
-			
+
+
 		});
 	}
 	/**
 	 * 是否自定义图片上传，替换默认
 	 */
-	_.uploadImg=function(){
-		
+	_.uploadImg = function() {
+
 		this.option.uploadImage && this.option.uploadImage(this.editor);
-		
+
 	}
 
 
@@ -346,4 +333,4 @@
 	}
 
 
-})();
+})(window,jQuery,ace,marked);
